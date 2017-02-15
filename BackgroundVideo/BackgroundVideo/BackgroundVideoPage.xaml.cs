50318a28641f5ad536f86340f89949a695327a26
Junian <git@junian.net>
2017-02-15 12:30:43 +0700

    init

---
+++ BackgroundVideo/BackgroundVideo/BackgroundVideoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BackgroundVideo
+{
+	public partial class BackgroundVideoPage : ContentPage
+	{
+		public BackgroundVideoPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
